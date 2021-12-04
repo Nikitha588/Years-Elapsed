b4d94b360858dd0eea5bd20b2303687b0bf3c2fc
nikitha588 <nikithac588@gmail.com>
2021-12-04 01:37:28 -0500

    1st commit

---
+++ src/elapsedYears/YearsElapsed.java
@@ -0,0 +1,22 @@
+package elapsedYears;
+
+public class YearsElapsed {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//[EASY]1. How many years elapsed between first and last movie of Harry Potter?
+				//declaring the values
+				int firstMovie = 2001;
+				int lastMovie = 2011;
+				
+				//formula to find the difference between first and last movie
+				int yearsElapsed = lastMovie - firstMovie;
+				
+				//Prints the statement
+				System.out.println("Total Number of years elapsed between first and last movie: " + yearsElapsed);
+				
+
+	}//End of Main
+
+}//End of Class
